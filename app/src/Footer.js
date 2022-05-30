5ed3fcdcaec5caac1f4bfda5fabc8008ea5e15c1
yuriikhomych <yuriykhomich@gmail.com>
2022-05-30 09:39:56 +0300

    Add frontend

---
+++ app/src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p className="text-center">
+                From Yurii Khomych
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
